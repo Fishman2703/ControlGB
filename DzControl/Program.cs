61e1405c636768e9d467aaa97116161040289aaa
олег рыбачук <fishman2703@yandex.ru>
2021-12-28 20:25:26 +0300

    add fun1

---
+++ DzControl/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] CreateArray(int count)
+{
+    return new int[count];
+}
+string PrintArray(int[] array)
+{
+    string res = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        res += $"{array[i]} ";
+    }
+    return res;
+}
