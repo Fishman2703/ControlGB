45205d58b5c754ddfe6c93dbfa5082839acf52d7
олег рыбачук <fishman2703@yandex.ru>
2021-12-28 20:27:20 +0300

    end

--- DzControl/Program.cs
+++ DzControl/Program.cs
@@ -26,3 +26,18 @@ Console.WriteLine(PrintArray(A));
 Console.WriteLine(); // разделить массивы 
 int index = 0;
 
+void FillArrayB(int[] array)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        if (A[i] % 2 == 0)
+        {
+            array[index] = A[i];
+            Console.Write($"{array[index]} ");
+            index++;
+        }
+    }
+}
+
+int[] B = CreateArray(A.Length);
+FillArrayB(B);
